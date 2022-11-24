1afd5cf931c3c5715ab371a27f679a8e526f673b
adelowomi <adelowomi@gmail.com>
2022-11-24 01:43:48 +0100

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         <ChakraProvider theme={theme}>
             <StyledThemeProvider>
                 <QueryClientProvider client={queryClient}>
+                    //@ts-nocheck
                     <Hydrate state={pageProps.dehydratedState}>
                         <RootStoreProvider>
                             <Component {...pageProps} />
