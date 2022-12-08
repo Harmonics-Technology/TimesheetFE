9e661c208375e5d8b78fc6ca612e01b259c0c54d
Adelowo Ajibola <adelowo.ajibola@suretree.com>
2022-12-08 20:04:57 +0100

    Fixed Layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -12,23 +12,31 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
     const router = useRouter();
+    const noSideBar =
+        router.pathname.startsWith("/login") ||
+        router.pathname.startsWith("/password") ||
+        router.pathname.startsWith("/forgot-password");
     const [openSidenav, setOpenSidenav] = useState(false);
 
     return (
         <>
-            <Flex pos="relative" p="1rem" bg="#f6f7f8">
-                <SideNav openSidenav={openSidenav} />
-                <Box w={["full", "82%"]} as="main" ml="auto" minH="95vh">
-                    <Box as="div" w="95%" mx="auto" mt="1rem" mb="3rem">
-                        <TopNav
-                            setOpenSidenav={setOpenSidenav}
-                            openSidenav={openSidenav}
-                        />
-                        <Box mt="2rem">{children}</Box>
-                        <Footer />
+            {!noSideBar ? (
+                <Flex pos="relative" p="1rem" bg="#f6f7f8">
+                    <SideNav openSidenav={openSidenav} />
+                    <Box w={["full", "82%"]} as="main" ml="auto" minH="95vh">
+                        <Box as="div" w="95%" mx="auto" mt="1rem" mb="3rem">
+                            <TopNav
+                                setOpenSidenav={setOpenSidenav}
+                                openSidenav={openSidenav}
+                            />
+                            <Box mt="2rem">{children}</Box>
+                            <Footer />
+                        </Box>
                     </Box>
-                </Box>
-            </Flex>
+                </Flex>
+            ) : (
+                <Box>{children}</Box>
+            )}
         </>
     );
 };
