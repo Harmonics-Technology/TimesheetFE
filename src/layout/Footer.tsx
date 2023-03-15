113b28f1877c25d6c978b5ce5d4373ecfd8c61e6
Adelowo Ajibola <adelowo.ajibola@suretree.com>
2023-03-15 18:52:52 +0100

    Fixed changes

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,4 +1,4 @@
-import { HStack, Text } from "@chakra-ui/react";
+import { HStack, Text } from '@chakra-ui/react';
 
 function Footer() {
     const year = new Date().getFullYear();
@@ -7,10 +7,12 @@ function Footer() {
             fontSize=".9rem"
             fontWeight="600"
             mt="1.5rem"
-            justify={["center", "flex-start"]}
+            justify={['center', 'flex-start']}
+            w="95%"
+            mx="auto"
         >
             <Text color="brand.300" mb="0">
-                &copy;{year}, Powered by{" "}
+                &copy;{year}, Powered by{' '}
             </Text>
             <Text color="brand.200" mb="0">
                 Proinsight Consulting
