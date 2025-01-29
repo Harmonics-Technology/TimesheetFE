a744553086cbf22291eb8693fb091b2964ddad48
Akinyemi Oluwabukunmi <braingrams40@gmail.com>
2025-01-29 20:58:05 +0000

    fix(pages): fixed document on admin

---
+++ pages/Admin/project-management/projects/[id]/documents.tsx
@@ -0,0 +1,61 @@
+import { DocumentsPage } from '@components/bits-utils/ProjectManagement/Projects/SingleProject/DocumentsPage';
+import { filterPagingSearchOptions } from '@components/generics/filterPagingSearchOptions';
+import { withPageAuth } from '@components/generics/withPageAuth';
+import { GetServerSideProps } from 'next';
+import React from 'react';
+import {
+    ProjectManagementService,
+    UserService,
+    UtilityService,
+} from 'src/services';
+
+const index = ({ id, project, files, users, currencies }) => {
+    return (
+        <DocumentsPage
+            id={id}
+            project={project}
+            files={files}
+            users={users}
+            currencies={currencies}
+        />
+    );
+};
+
+export default index;
+
+export const getServerSideProps: GetServerSideProps = withPageAuth(
+    async (ctx: any) => {
+        const superAdminId = JSON.parse(ctx.req.cookies.user).id;
+        const pagingOptions = filterPagingSearchOptions(ctx);
+        const { id } = ctx.query;
+        try {
+            const data = await ProjectManagementService.getProject(id);
+            const files = await ProjectManagementService.listAttachments(id);
+            const users = await UserService.listUsersByRoles(
+                superAdminId,
+                'team member,super admin,admin,client,supervisor',
+            );
+            const currencies = await UtilityService.listCountries();
+
+            console.log({ id });
+
+            return {
+                props: {
+                    project: data.data,
+                    id,
+                    files: files.data,
+                    users: users.data,
+                    currencies: currencies.data,
+                },
+            };
+        } catch (error: any) {
+            console.log({ error });
+            return {
+                props: {
+                    data: [],
+                    id,
+                },
+            };
+        }
+    },
+);
